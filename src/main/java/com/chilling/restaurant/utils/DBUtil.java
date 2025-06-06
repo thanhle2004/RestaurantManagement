93c098317701524e38e8809773d1837edfdb1758
chamnekkk <tramphannguyenhuyen@gmail.com>
2025-06-06 23:25:10 +0700

    Update DBUtil.java

--- src/main/java/com/chilling/restaurant/utils/DBUtil.java
+++ src/main/java/com/chilling/restaurant/utils/DBUtil.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 
 public class DBUtil {
     //Use Railway 
-    //private static final String JDBC_URL = "jdbc:mysql://root:tkHrEwMWNpuLYlPvVviJuqgRZNViJZMl@shinkansen.proxy.rlwy.net:51671/railway";
-    //private static final String JDBC_USER = "root";
-    //private static final String JDBC_PASSWORD = "tkHrEwMWNpuLYlPvVviJuqgRZNViJZMl";
+    private static final String JDBC_URL = "jdbc:mysql://root:WMchuNXWznydqIeQCxwRmWxFYfKrVdJT@ballast.proxy.rlwy.net:21986/railway";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "WMchuNXWznydqIeQCxwRmWxFYfKrVdJT";
     
     //Use Local
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/restaurantdb"; 
-    private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "123456"; 
+    //private static final String JDBC_URL = "jdbc:mysql://localhost:3306/restaurantdb"; 
+    //private static final String JDBC_USER = "root";
+    //private static final String JDBC_PASSWORD = "123456"; 
 
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
